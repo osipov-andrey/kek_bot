589422ef7861b1b1a0719f5b9028e1ec860504c6
osipov-andrey <osyapov@bk.ru>
2021-01-22 19:37:05 +0300

    Initial3

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
 import asyncio
 import logging
 import random
+from urllib.parse import urljoin
 
 import aiogram
 from aiogram import types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.webhook import get_new_configured_app
+from aiohttp import web
+
+logging.basicConfig(
+    level=logging.INFO,
+    handlers=[
+        logging.FileHandler(filename="log.log")
+    ],
+)
 
 
-logging.basicConfig(level=logging.INFO)
 
 
-_API_TOKEN = "1546857864:AAEPBXnsuVVPTJxzrPM8l4g-lQgDLh867T0"
+API_TOKEN = "1546857864:AAEPBXnsuVVPTJxzrPM8l4g-lQgDLh867T0"
+
+
+WEBHOOK_HOST = "https://gentle-brook-78003.herokuapp.com/"
+WEBHOOK_URL_PATH = '/webhook/' + API_TOKEN
+WEBHOOK_URL = urljoin(WEBHOOK_HOST, WEBHOOK_URL_PATH)
+
 STIKER_SETS = [
     "Gachimuchi_pack",
     "Derpingpack",
     "Beruhmt"
 ]
-_bot = aiogram.Bot(token=_API_TOKEN)
+bot = aiogram.Bot(token=API_TOKEN)
 state_storage = MemoryStorage()
-telegram_api_dispatcher = aiogram.Dispatcher(bot=_bot, storage=state_storage)
+telegram_api_dispatcher = aiogram.Dispatcher(bot=bot, storage=state_storage)
 
 
 STIKERS = list()
@@ -29,7 +44,7 @@ async def get_stikers():
     global STIKERS
     all_stikers = list()
     for set in STIKER_SETS:
-        stikers = await _bot.get_sticker_set(set)
+        stikers = await bot.get_sticker_set(set)
         stikers = stikers.stickers
         all_stikers.extend(stikers)
     STIKERS = all_stikers
@@ -52,7 +67,7 @@ async def check(message: types.Message, state: FSMContext):
     stiker = random.choice(STIKERS)
     chat = message.chat
 
-    await _bot.send_sticker(
+    await bot.send_sticker(
         reply_to_message_id=message.message_id,
         chat_id=chat.id,
         sticker=stiker.file_id
@@ -60,7 +75,20 @@ async def check(message: types.Message, state: FSMContext):
     await state.reset_state()
 
 
-loop = asyncio.get_event_loop()
-loop.create_task(get_stikers())
-loop.create_task(telegram_api_dispatcher.start_polling())
-loop.run_forever()
+async def on_startup(app):
+    """Simple hook for aiohttp application which manages webhook"""
+    await bot.delete_webhook()
+    await bot.set_webhook(WEBHOOK_URL)
+    await get_stikers()
+    asyncio.ensure_future(telegram_api_dispatcher.start_polling())
+
+
+if __name__ == '__main__':
+    app = get_new_configured_app(dispatcher=telegram_api_dispatcher, path=WEBHOOK_URL_PATH)
+    app.on_startup.append(on_startup)
+    web.run_app(app, host="0.0.0.0", port=8080)
+#
+# loop = asyncio.get_event_loop()
+# loop.create_task(get_stikers())
+# loop.create_task(telegram_api_dispatcher.start_polling())
+# loop.run_forever()
